266d95db6ac339f004e7945f2f13e84fce9557f2
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2017-03-08 21:37:09 -0500

    cleanup arg handling

--- lockbox/__main__.py
+++ lockbox/__main__.py
@@ -8,19 +8,16 @@ from contextlib import closing
 from .lib import LockBox, commit_lockbox, get_secret, generate_random, serialize
 
 ENV_KEY = "LOCKBOX_SECRET"
-
+CMD_PREFIX = "cmd_"
 
 def cmd_set(lockbox, args):
     """
     Add a key to the lockbox
     """
     v = get_secret("v? ")
-
     if not v:
         raise ValueError("abort")
-
     lockbox.set(args.key, v)
-
     store_lockbox(args.lockbox, lockbox)
 
 
@@ -39,6 +36,7 @@ def cmd_gen(lockbox, args):
     lockbox.set(args.key, v)
     store_lockbox(args.lockbox, lockbox)
 
+
 def cmd_dump(lockbox, args):
     """
     Dump plain-text lockbox as JSON
@@ -47,35 +45,38 @@ def cmd_dump(lockbox, args):
     f = StringIO.StringIO()
     serialize(doc, f)
     print f.getvalue()
+
     
-CMDS = [("set", cmd_set), ("get", cmd_get), ("gen", cmd_gen), ("dump", cmd_dump)]
+def cmd_import(lockbox, args):
+    doc = json.load(sys.stdin)
+    
+
+    
+CMDS = {k[len(CMD_PREFIX):]: v for k, v in locals().items() if k.startswith(CMD_PREFIX) and callable(v)}
 
 def parse_args():
     parser = ArgumentParser(prog="lockbox")
     subparsers = parser.add_subparsers()
 
-    def add_subparser(name, func):
+    def add_subparser(name, key=False, stdin=False):
+        func = CMDS[name]
         subparser = subparsers.add_parser(name, help=func.__doc__)
         subparser.add_argument("lockbox", help="path to lockbox")
+        if takes_key:
+            subparser.add_argument("key", help="key to operate on")
+        if takes_stdin:
+            subparser.add_argument("--stdin", action="store_true", help="load dropbox from stdin")
         subparser.set_defaults(func=func)
         return subparser
-
-    subparser_set, subparser_get, subparser_gen, subparser_dump = [add_subparser(name, func) for name, func in CMDS]
-
-    for subparser in (subparser_set, subparser_get, subparser_gen):
-        subparser.add_argument("key", help="key to operate on")
+    
+    subparsers_set = add_subparser("set", key=True, stdin=True)
+    subparsers_get = add_subparser("get", key=True, stdin=True)
+    subparsers_gen = add_subparser("gen", key=True, stdin=True)
+    subparsers_dump = add_subparser("dump")
+    subparsers_import = add_subparser("import")
 
     args = parser.parse_args()
     return args
-
-def open_lockbox(path):
-    f = None
-    if path == "-":
-        f = sys.stdin
-    else:
-        if os.path.exists(path):
-            f = open(path, mode=mode)
-    return f
 
 
 def store_lockbox(path, lockbox):
@@ -87,18 +88,22 @@ def store_lockbox(path, lockbox):
 
 def main():
     args = parse_args()
-
     secret = os.environ[ENV_KEY] if ENV_KEY in os.environ else get_secret("secret? ")
-
     lockbox = LockBox(secret)
 
-    f = open_lockbox(args.lockbox)
+    if args.stdin:
+        f = sys.stdin
+    elif os.path.exists(args.lockbox):
+        f = open(args.lockbox, mode=mode)
+    else:
+        f = None
+
     if f:
         with closing(f):
             lockbox.load(f)
-
     args.func(lockbox, args)
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
